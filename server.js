1dde6e08d6f3f8812cff491debfc2ea50f6617b7
JacksonWhite4725 <jawh4725@colorado.edu>
2021-09-13 16:22:52 -0600

    Added POST request

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { resolveSoa } = require('dns');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { readAndAppend } = require('../../01-Activities/28-Stu_Mini-Project/Main/helpers/fsUtils');
 
 // Assign port to use
 const PORT = process.env.port || 3001;
@@ -24,6 +25,14 @@ app.get('/notes', (req, res) =>
 app.get('/api/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/db/db.json'))
 );
+
+// POST API route for JSON note. ID generation logic taken from W3 schools. POST request logic taken from codeforgeek (https://codeforgeek.com/handle-get-post-request-express-4/) and activities in module 11.
+app.post('api/notes', (req, res) => {
+    const {title, text} = req.body;
+    const newNote = {title, text, id: Math.floor(Math.random() * 101)};
+    readAndAppend(newNote, '/db/db.json');
+    res.end('Note added successfully!');
+});
 
 // GET route for homepage (logic borrowed from instructor solution of mini project 11)
 app.get('*', (req, res) =>
